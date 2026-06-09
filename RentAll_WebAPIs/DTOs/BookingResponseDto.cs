703a18a8db17a2073ec2951269db5cec76802bee
AdeMaq <160094094+AdeMaq@users.noreply.github.com>
2026-06-09 15:27:38 +0500

    Handling Renting and updation of availability status

--- RentAll_WebAPIs/DTOs/BookingResponseDto.cs
+++ RentAll_WebAPIs/DTOs/BookingResponseDto.cs
@@ -13,5 +13,8 @@
         public decimal DepositAmount { get; set; }
 
         public string Status { get; set; } = string.Empty;
+
+        public DateTime StartDate { get; set; }  
+        public DateTime EndDate { get; set; }  
     }
 }
